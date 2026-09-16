bfc3d9fd2d0397fd460e850f21dbb7ce9f081340
nico <yolomaster2@web.de>
2026-09-16 13:11:48 +0200

    testing  self lock

--- kettu-plugin/src/index.js
+++ kettu-plugin/src/index.js
@@ -1845,7 +1845,7 @@ function ConfigPanel(props) {
             button("Save Relay URL", saveRelayUrl)
         )) : null,
         
-        (!isOwnProfile && canViewRemote) ? section("Sub Contol", "Sub Control", h(
+        (isOwnProfile && canViewRemote) ? section("Sub Contol", "Sub Control", h(
             View,
             null,
             h(Text, { style: { color: "#b5bac1", marginTop: 6 } }, `${editableConfig.rules_groups.length} group(s), ${editableConfig.rules.length} rule(s)`),
